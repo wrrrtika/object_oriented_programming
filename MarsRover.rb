3d8219174c8d2660360d72cb80fa2d52d868b29c
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-24 08:53:18 -0400

    Large Rehaul. user inputs now work, two rovers available.

--- MarsRover.rb
+++ MarsRover.rb
@@ -1,83 +1,95 @@
-
-
-# puts "Enter your rover instructions:"
-# roverinput = gets.chomp
-
-
 class Rover
-	attr_accessor :x, :y, :direction 
+	attr_accessor :x, :y, :direction
 
-	def initialize(x, y, direction)
+	def initialize (x, y, direction)
 		@x = x
 		@y = y
 		@direction = direction
+
 	end
 
-	def read(instruction)
-		if instruction == "M"
-			move
-		elsif instruction == "L"
-			turnleft
-		elsif instruction == "R"
-			turnright
-		else 
-			puts "Please input a valid instruction."
+	def display
+		puts "Rover is at #{@x}, #{@y}, facing #{@direction}."
 	end
-	end
-	def move 
-		if @direction == "N"
+
+	def move
+		if (@direction == "N") 
 			@y += 1
-		elsif @direction == "S"
+		elsif (@direction == "S")
 			@y -= 1
-		elsif @direction == "W"
-			@x -= 1 
-		elsif @direction == "E"
-			@x +=1 
-		else
-			puts "please enter a valid location"
-	end
-
-	end
-
-	def turnleft
-		if @direction == "N"
-			@direction = "W"
-		elsif @direction== "W"
-			@direction = "S"
-		elsif @direction == "S"
-			@direction = "E"
-		elsif @direction == "E"
-			@direction = "N"
-		else
-			puts "Please enter a valid direction"
+		elsif (@direction == "W")
+			@x -= 1
+		else (@direction == "E")
+			@x += 1
 		end
-
 	end
 
-	def turnright
-		if @direction == "N"
-			@direction = "E"
-		elsif @direction== "E"
-			@direction = "S"
-		elsif @direction == "S"
+	def turn_left
+		if (@direction == "N")  
 			@direction = "W"
-		elsif @direction == "W"
+		elsif (@direction == "W")  
+			@direction = "S"
+		elsif (@direction == "S") 
+			@direction = "E"
+		else (@direction == "E")
 			@direction = "N"
-		else
-			puts "Please enter a valid direction"
-		
+		end
+	end
+	
+	def turn_right
+		if (@direction == "N")  
+			@direction = "E"
+		elsif (@direction == "E")  
+			@direction = "S"
+		elsif (@direction == "S") 
+			@direction = "W"
+		else (@direction == "W") 
+			@direction = "N"
 		end
 	end
 
 
-	
-	
+	def execute_instructions
+		@instructions.each_char do |x|
+			case x
+			when "M"
+				move
+			when"L"
+				turn_left
+			when"R"
+				turn_right
+			end
+		end
+	end
 
+	def read_instructions (instructions)
+		@instructions = instructions
+		execute_instructions
+	
+	end
 end
 
-rover = Rover.new(0,0,"N")
+puts "where is Curiousity (x, y, direction)?"
+rover_position = gets.chomp.upcase
 
-rover.read("R")
+Curiousity = Rover.new(rover_position.split[0].to_i,rover_position.split[1].to_i,rover_position.split[2])
+Curiousity.display
 
+puts "Input instructions (M, L, R):"
+	instructions = gets.chomp.upcase
+Curiousity.read_instructions(instructions)
+Curiousity.display
 
-puts "Rover's current location is #{rover.x}, #{rover.y}, facing #{rover.direction}"
+puts "where is Opportunity (x, y, direction)?"
+rover_position = gets.chomp.upcase
+
+Opportunity = Rover.new(rover_position.split[0].to_i,rover_position.split[1].to_i,rover_position.split[2])
+Opportunity.display
+
+puts "Input instructions (M, L, R):"
+	instructions = gets.chomp.upcase
+Opportunity.read_instructions(instructions)
+Opportunity.display
+
+# puts "Would you like to input more instructions? Y/N"
+# 	if Y
